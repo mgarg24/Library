8d1f5c07c029b9f2415191a6d4624dd51dd90be1
Manur Garg <mgarg8@uwo.ca>
2023-03-14 00:36:13 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+  // the constructor...
+
+  this.title= title;
+  this.author =author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+const btn = document.querySelector(".add");
+
+btn.addEventListener('click', () => {});
+
+
