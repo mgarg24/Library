34450bd4fd93dd66a3af31e06f3ca5734dc7891e
Manur Garg <mgarg8@uwo.ca>
2023-05-03 20:23:41 -0400

    added form

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let myLibrary = [];
+let clear = document.querySelector(".clear");
+let main = document.querySelector(".main");
+
 
 function Book(title, author, pages) {
   // the constructor...
@@ -8,12 +11,42 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, pages) {
+  let book = new Book(title, author, pages);
+  myLibrary.push(book);
 }
 
 const btn = document.querySelector(".add");
 
-btn.addEventListener('click', () => {});
+btn.addEventListener('click', function() {
+
+    
+    main.style.display = 'block';
+    
+});
+
+let form = document.querySelector(".form");
+
+form.addEventListener ('submit', function(e) {
+
+  e.preventDefault();
+  const title = document.getElementById("title").value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  
+  if (title === "" || author === "" || pages === "") {
+    alert("Please fill in all fields");
+    return false;
+  }
+  let formData = document.createElement('div');
+  formData.className = 'formdata';
+  
+  formData.innerHTML = title;
+  main.appendChild(formData);
+
+  addBookToLibrary(title,author,pages);
+
+  alert(myLibrary);
 
 
+});
