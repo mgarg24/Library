dbf9d9e14ced01151f119d067344ea33ac352072
Manur Garg <mgarg8@uwo.ca>
2023-05-07 01:40:43 -0400

    need to fix read btn

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let myLibrary = [];
 let clear = document.querySelector(".clear");
 let main = document.querySelector(".main");
+let cancel = document.querySelector(".cancel");
 
+let library = document.querySelector(".books");
 
 function Book(title, author, pages) {
   // the constructor...
@@ -13,8 +15,81 @@ function Book(title, author, pages) {
 
 function addBookToLibrary(title, author, pages) {
   let book = new Book(title, author, pages);
+  
   myLibrary.push(book);
+  
 }
+
+function displayBooks () {
+
+  library.innerHTML = '';
+
+  // Loop through each book in the library
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+
+    // Create a new div element to hold the book card
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('bookcard');
+
+    // Add the book details to the card
+    const title1 = document.createElement('h2');
+    title1.innerHTML =  `${book.title}`;
+    bookCard.appendChild(title1);
+    
+    const author = document.createElement('p');
+    author.textContent = `Author: ${book.author}`;
+    bookCard.appendChild(author);
+
+    const pages = document.createElement('p');
+    pages.textContent = `Pages: ${book.pages}`;
+    bookCard.appendChild(pages);
+
+    const read = document.createElement('p');
+    read.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+    bookCard.appendChild(read);
+
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', function() {
+      
+      myLibrary.splice(i, 1);
+      
+      displayBooks();
+    });
+    
+    bookCard.appendChild(removeBtn);
+    const toggleButton = document.createElement('button');
+    toggleButton.textContent = book.read ? 'Read' : 'Unread';
+    toggleButton.addEventListener('click', function() {
+    book.read = !book.read;
+    toggleButton.textContent = book.read ? 'Read' : 'Unread';
+    });
+
+
+     bookCard.appendChild(toggleButton);
+
+
+    library.appendChild(bookCard);
+  }
+}
+
+let check = document.querySelector("input[name=read]");
+
+check.addEventListener('change', function(){
+
+  if (this.checked) {
+    toggleButton.textContent = "read";
+
+  } else {
+    console.log("Checkbox is not checked..");
+  }
+
+});
+
+
+
+
 
 const btn = document.querySelector(".add");
 
@@ -30,23 +105,35 @@ let form = document.querySelector(".form");
 form.addEventListener ('submit', function(e) {
 
   e.preventDefault();
+  
   const title = document.getElementById("title").value;
+  
   let author = document.getElementById('author').value;
   let pages = document.getElementById('pages').value;
   
-  if (title === "" || author === "" || pages === "") {
+  /*if (title === "" || author === "" || pages === "") {
     alert("Please fill in all fields");
     return false;
-  }
-  let formData = document.createElement('div');
+  }*/
+  /*let formData = document.createElement('div');
   formData.className = 'formdata';
   
   formData.innerHTML = title;
-  main.appendChild(formData);
+  main.appendChild(formData);*/
+
+  
 
   addBookToLibrary(title,author,pages);
 
-  alert(myLibrary);
-
+  main.style.display = 'none';
+  displayBooks();
+  form.reset();
 
 });
+
+cancel.addEventListener('click' , function(){
+
+  main.style.display = 'none';
+
+});
+
